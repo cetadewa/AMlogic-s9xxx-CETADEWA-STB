52672384dfd912a7f809c2c0638e72acef0d3262
cetadewa <106055033+cetadewa@users.noreply.github.com>
2022-05-24 10:23:49 +0700

    Update diy-part2.sh

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -44,7 +44,8 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/o
 pushd package/openwrt-openclash/tools/po2lmo && make && sudo make install 2>/dev/null && popd
 
 Add luci-app-ssr-plus
-git clone https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/openwrt-ssrplus
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/openwrt-ssrplus
+rm -rf package/openwrt-ssrplus/luci-app-ssr-plus/po/zh_Hans 2>/dev/null
 
 # Add p7zip
 # svn co https://github.com/hubutui/p7zip-lede/trunk package/p7zip
